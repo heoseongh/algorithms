a7ed3dbf9a0cae0d4cd245cd2e16ef696d1c54b9
heoseongh <heoseongh>
2020-05-12 02:35:39 +0900

    last commit

---
+++ bubble_sort.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define NUM 10
+
+int main(void){
+	int i, j, temp;
+	int array[NUM] = {1, 10, 5, 8, 7, 6, 4, 3, 2 ,9};
+	for(i = 0; i < NUM; i++){
+		for(j = 0; j <NUM - i - 1; j++){	//한 번만 실행해도 제일 큰 값을 찾을 수 있다. 따라서 마지막 숫자는 굳이 비교할 필요가 없다.
+			if(array[j] > array[j+1]){
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
+	for(i = 0; i < NUM; i++){
+		printf("%d ", array[i]);
+	}
+	return 0;
+}
