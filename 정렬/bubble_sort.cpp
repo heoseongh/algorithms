984c18f1c938cbf4cde80713d6dcb27a95131844
heoseongh <heoseongh>
2020-05-12 03:46:05 +0900

    200511

--- 정렬/bubble_sort.cpp
+++ 정렬/bubble_sort.cpp
@@ -5,7 +5,7 @@ int main(void){
 	int i, j, temp;
 	int array[NUM] = {1, 10, 5, 8, 7, 6, 4, 3, 2 ,9};
 	for(i = 0; i < NUM; i++){
-		for(j = 0; j <NUM - i - 1; j++){	//한 번만 실행해도 제일 큰 값을 찾을 수 있다. 따라서 마지막 숫자는 굳이 비교할 필요가 없다.
+		for(j = 0; j <NUM - i - 1; j++){	
 			if(array[j] > array[j+1]){
 				temp = array[j];
 				array[j] = array[j+1];
@@ -18,3 +18,6 @@ int main(void){
 	}
 	return 0;
 }
+// 두 번째 for문에서 NUM - i - 1 로 설정해준 이유는
+// 한 번만 실행해도 제일 큰 값을 찾을 수 있다.
+// 따라서 마지막 숫자는 굳이 비교할 필요가 없다.
--- 정렬/selection_sort.cpp
+++ 정렬/selection_sort.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#define NUM 10
 
 int main(void){
 	int i, j, min, index, temp;
-	int array[10] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
-	for(i = 0; i < 10; i++){
+	int array[NUM] = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
+	for(i = 0; i < NUM; i++){
 		min = 9999;		//모든 원소들 보다 큰 임의의 수를 넣어준다. 
-		for(j = i; j < 10; j++){
+		for(j = i; j < NUM; j++){	//시작점의 위치를 설정해준다. 
 			if(min > array[j]){
 				min = array[j];
 				index = j;
